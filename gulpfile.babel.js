a9e6a74d0db61d56ea898b69d5511ec8b81bc530
doug <linderd@iinet.net.au>
2015-10-27 21:40:36 +0800

    Adds files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,4 @@
 import gulp  from 'gulp';
-import toml  from 'gulp-toml';
 import rename from 'gulp-rename';
 import insert from 'gulp-insert';
 import shell from 'gulp-shell';
@@ -8,26 +7,26 @@ import unity from 'gulp-unity';
 import p from 'path';
 
 var config = {
-  cs: p.join(__dirname, 'Assets', 'packages'),
-  toml: p.join(__dirname, 'Assets', 'packages'),
-  json: p.join(__dirname, 'Assets', 'build')
+  spriter: p.join(__dirname, 'SpriterDotNet', 'SpriterDotNet'),
+  assets: p.join(__dirname, 'Assets', 'SpriterDotNetCore'),
 };
 
 gulp.task('default', function(callback) {
-  run('data', 'code', callback);
+  run('deploy', 'test', callback);
 });
 
-// Compile and run tests
-gulp.task('code', function(callback) {
-  run('tests', callback);
+// Copy source into assets folder
+gulp.task('deploy', function() {
+  return gulp.src(`${config.spriter}/**/*.cs`)
+    .pipe(gulp.dest(config.assets));
 });
 
 // Run unity tests
-gulp.task('tests', function() {
+gulp.task('test', function() {
   return gulp.src('./package.json')
     .pipe(unity({
       method: 'N.TestRunner.Run',
-      args: ['--filterTests=Air'],
+      args: ['--filterTests=Spriter'],
       debug: (v) => {
         unity.debug(v, [
           { pattern: /.*DEBUG.*/, color: 'yellow', context: 0 },
@@ -37,90 +36,4 @@ gulp.task('tests', function() {
         ])
       }
     }));
-});
-
-// Run unity tests
-gulp.task('dev', function() {
-  return gulp.src('./package.json')
-    .pipe(unity({
-      method: 'N.TestRunner.Run',
-      args: ['--filterTests=Combat'],
-      debug: (v) => {
-        unity.debug(v, [
-          { pattern: /\! Test.*/, color: 'red' },
-          { pattern: /\- Test.*/, color: 'green' },
-          { pattern: /test_.*/, color: 'red', context: true },
-          { pattern: /.*DEBUG.*/, color: 'yellow', context: 0 },
-          { pattern: /.*ERROR.*/, color: 'red', context: 0 },
-        ])
-      }
-    }));
-});
-
-// Run unity tests
-gulp.task('n', function() {
-  return gulp.src('./package.json')
-    .pipe(unity({
-      method: 'N.TestRunner.Run',
-      args: ['--filterTests=N.'],
-      debug: (v) => {
-        unity.debug(v, [
-          { pattern: /\! Test.*/, color: 'red' },
-          { pattern: /\- Test.*/, color: 'green' },
-          { pattern: /test_.*/, color: 'red', context: true },
-          { pattern: /.*DEBUG.*/, color: 'yellow', context: 0 },
-          { pattern: /.*ERROR.*/, color: 'red', context: 0 },
-        ])
-      }
-    }));
-});
-
-// Build files
-gulp.task('data', function(callback) {
-  run('toml', 'manifest', callback);
-});
-
-// TOML -> JSON
-gulp.task('toml', function() {
-  return gulp.src(config.toml + '/**/*.toml')
-    .pipe(insert.transform((contents, file) => {
-      var parts = file.path.split(p.sep);
-      var name = parts[parts.length - 1];
-      name = name.split(".")[0];
-      return "id = \"" + name + "\"\n" + contents;
-    }))
-    .pipe(toml())
-    .pipe(rename((path) => {
-      var parts = path.dirname.split(p.sep);
-      var new_parts = [parts[0], 'Resources', parts[0]];
-      for (var i = 2; i < parts.length; ++i) {
-        new_parts.push(parts[i]);
-      }
-      path.dirname = p.join.apply(p, new_parts);
-    }))
-    .pipe(gulp.dest(config.json));
-});
-
-// Rebuild a new valid manifest file
-gulp.task('manifest', function() {
-  return gulp.src('./package.json')
-    .pipe(unity({
-      method: 'N.Package.Data.ManifestBuilder.Run',
-      debug: (v) => {
-        unity.debug(v, [
-          { pattern: /System.Exception/, color: 'red', context: true }
-        ])
-      }
-    }));
-});
-
-// Watch for data changes
-gulp.task('watch', function() {
-  gulp.watch("./Assets/packages/*/src/**/*.toml", ['default']);
-  gulp.watch("./Assets/packages/*/src/**/*.cs", ['tests']);
-});
-
-// Live changes for when unity is running
-gulp.task('live', function() {
-  gulp.watch("./Assets/packages/*/src/**/*.toml", ['toml']);
 });
